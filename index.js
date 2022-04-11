07e916fc4879781f577cd4dc7fe7e5a1798eb383
Soumya Nayar <nayar.s@northeastern.edu>
2022-04-10 17:54:42 -0700

    Lambda changes

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 const AWS = require("aws-sdk");
-// Set the region
-AWS.config.update({ region: "us-west-2" });
+
+const checkIfEmailSentAlready = async (
+  dynamoDbClient,
+  emailTrackingDynamoDBTable,
+  userEmail
+) => {
+  const params = {
+    TableName: emailTrackingDynamoDBTable,
+    Key: {
+      email: userEmail,
+    },
+  };
+  const data = await dynamoDbClient.get(params).promise();
+  console.log("Data:", data);
+  if (data.Item) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const logEmailSentToDynamoDB = async (
+  dynamoDbClient,
+  emailTrackingDynamoDBTable,
+  userEmail
+) => {
+  const params = {
+    TableName: emailTrackingDynamoDBTable,
+    Item: {
+      email: userEmail,
+    },
+  };
+  const data = await dynamoDbClient.put(params).promise();
+  console.log("Data:", data);
+};
 
 exports.handler = async (event, context, callback) => {
   console.log("Received event:", JSON.stringify(event, null, 4));
+  const emailTrackingDynamoDBTable = process.env.EmailTrackingDynamoDBTable;
+  const emailTrackingDynamoDBRegion = process.env.EmailTrackingDynamoDBRegion;
+
+  // Set the region
+  AWS.config.update({ region: emailTrackingDynamoDBRegion });
+
+  const dynamoDbClient = new AWS.DynamoDB.DocumentClient({
+    region: emailTrackingDynamoDBRegion,
+  });
 
   const message = event.Records[0].Sns.Message;
   console.log("Message received from SNS:", message);
@@ -21,30 +63,52 @@ exports.handler = async (event, context, callback) => {
   const last_name = parsedMessage.last_name;
   console.log("Last name:", last_name);
 
-  // Send email using AWS SES
-  const ses = new AWS.SES();
-  const params = {
-    Destination: {
-      ToAddresses: [userEmail],
-    },
-    Message: {
-      Body: {
-        Html: {
-          Charset: "UTF-8",
-          Data: `<p>Hello ${first_name} ${last_name},</p>
+  const emailAlreadySent = await checkIfEmailSentAlready(
+    dynamoDbClient,
+    emailTrackingDynamoDBTable,
+    userEmail
+  );
+
+  if (!emailAlreadySent) {
+    // Send email using AWS SES
+    console.log(
+      "Email is not already sent to the user: " + userEmail + ". Trying to send"
+    );
+    const ses = new AWS.SES();
+    const params = {
+      Destination: {
+        ToAddresses: [userEmail],
+      },
+      Message: {
+        Body: {
+          Html: {
+            Charset: "UTF-8",
+            Data: `<p>Hello ${first_name} ${last_name},</p>
             <p>To verify your email address with demo.soumyanayar.me, Please click the following link: <a href="http://demo.soumyanayar.me/v1/verifyUserEmail?email=${userEmail}&token=${userToken}">Verify Email</a> or paste the following link in the browser: http://demo.soumyanayar.me/v1/verifyUserEmail?email=${userEmail}&token=${userToken}</p>`,
+          },
+        },
+        Subject: {
+          Charset: "UTF-8",
+          Data: "Verify you user account for demo.soumyanayar.me",
         },
       },
-      Subject: {
-        Charset: "UTF-8",
-        Data: "Verify you user account for demo.soumyanayar.me",
-      },
-    },
-    Source: "userverification@demo.soumyanayar.me",
-  };
+      Source: "userverification@demo.soumyanayar.me",
+    };
 
-  const data = await ses.sendEmail(params).promise();
-  console.log(data);
-  console.log("Email sent successfully");
+    const data = await ses.sendEmail(params).promise();
+    console.log(data);
+    console.log("Email sent successfully");
+
+    await logEmailSentToDynamoDB(
+      dynamoDbClient,
+      emailTrackingDynamoDBTable,
+      userEmail
+    );
+    console.log("Email logged to DynamoDB");
+  } else {
+    console.log(
+      "Email already sent to user: " + userEmail + " No need to send again"
+    );
+  }
   callback(null, "success");
 };
